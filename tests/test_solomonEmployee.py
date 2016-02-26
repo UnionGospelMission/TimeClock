aff279d046678cf53e95bbc6e5f4ecc441f7a76a
Logan Perkins <lperkins2@zagmail.gonzaga.edu>
2016-02-26 15:34:05 -0800

    unit test for SE

--- tests/test_solomonEmployee.py
+++ tests/test_solomonEmployee.py
@@ -7,16 +7,16 @@ from TimeClock.Solomon.SolomonEmployee import SolomonEmployee
 from TimeClock.Util import NULL
 
 
+e = IEmployee(NULL)
+e.employee_id = 1001
+
+
 class SolomonTester(TestCase):
     def testSEmployee(self):
-        e = IEmployee(NULL)
-        e.employee_id = 1001
         se = ISolomonEmployee(e)
         self.assertEqual(se.defaultArea.name, 'General and Administrative')
 
     def testSEBenefits(self):
-        e = IEmployee(NULL)
-        e.employee_id = 1002
         se = ISolomonEmployee(e)
         print(19, se.getBenefits())
 
