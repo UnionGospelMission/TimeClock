ead30738bd3cae9855798830ab1f816ea5f5502b
Logan Perkins <lperkins2@zagmail.gonzaga.edu>
2016-03-30 02:49:53 -0700

    AD authentication stub

--- src/TimeClock/AD/__init__.py
+++ src/TimeClock/AD/__init__.py
@@ -3,6 +3,9 @@ from TimeClock.Utils import coerce
 
 
 @coerce
-def authenticate(employee: IEmployee, pw: bytes) -> IEmployee:
-    if employee.alternate_authentication:
-        return employee.alternate_authentication
+def authenticate(employee: IEmployee, pw: str):
+    from ldap3 import Server, Connection, ALL, NTLM
+    server = Server('ipa.demo1.freeipa.org')
+    conn = Connection(server, user="Domain\\User", password=pw, authentication=NTLM)
+    conn.extend.standard.who_am_i()
+
